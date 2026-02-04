db7ac87b120883fa0c703b08d0b4643e1b92b981
ColtonPaulBadock <hockey9192008@gmail.com>
2026-02-04 14:34:32 -0800

    Intialized the project for the RF transmitter/controller system. Going to build a class/header library that can be imported and will be able to recieve and transmit via 315mhz without issue and allow for easier interfacing with the transmitter/reciever.

---
+++ source/RcBoatRFControllerSystem/src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+
+/*
+Author: Colton Paul Badock
+Date: Feb 2026
+Version: 1
+
+DESCRIPTION:
+A system to control the boat via recieved radio freqencies.
+This system is the controller and will send inputs transmitted at 315 MHz to the RC-Boat.
+There will be a onboard recieve picking up these inputs, it likely will not transmit back
+*/
+
+
+
+//Init function (constructor)
+void setup() {
+  
+}
+
+
+//Infinite application loop
+void loop() {
+  
+}
