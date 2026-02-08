be5fbb4c3401c7932510f360e1ae1a2f55667450
ColtonPaulBadock <hockey9192008@gmail.com>
2026-02-07 17:34:41 -0800

    Version 2; Got the transmitter code possibly working. Need to write a reciever function and possibly run a second board to test recieption.

--- source/RcBoatRFControllerSystem/src/main.cpp
+++ source/RcBoatRFControllerSystem/src/main.cpp
@@ -1,9 +1,11 @@
 #include <Arduino.h>
+#include <RH_ASK.h>
+#include <SPI.h>
 
 /*
 Author: Colton Paul Badock
 Date: Feb 2026
-Version: 1
+Version: 2
 
 DESCRIPTION:
 A system to control the boat via recieved radio freqencies.
@@ -12,14 +14,53 @@ There will be a onboard recieve picking up these inputs, it likely will not tran
 */
 
 
+//The transmitter for the user.
+//This transmitter system will enable 433 MHz
+//communication from the users controller
+//to the onboard reciever, allowing
+//for wireless control of the boat
+RH_ASK user_transmitter;
+
+
+
+//Transmits a message/command on the
+//open air at 433MHz. The message
+//should be recieved by the RC-Boat
+//and the command will be interprated by
+//the boards micro controller
+void transmitMessage(char *msg) {
+    
+    
+    //const char *msg = "Hello World!";
+    user_transmitter.send((uint8_t *)msg, strlen(msg));
+    user_transmitter.waitPacketSent();
+}
+
+
 
 //Init function (constructor)
 void setup() {
   
+    //Start serial communication
+    //back to the PC (if plugged in)
+    //for debugging
+    Serial.begin(9600);
+
+    //If the transmitter failed to intialize,
+    //throw an error back to serial
+    if (!user_transmitter.init()) {
+        Serial.println("Transmitter failed to intialize; Error Code: 01");
+    }
+
 }
 
 
 //Infinite application loop
 void loop() {
   
+    transmitMessage("Hello World!");
+
+    delay(5000);
+
+    Serial.println("Successfully transmitted??");
 }
