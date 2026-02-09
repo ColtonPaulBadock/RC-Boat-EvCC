42ec50aae6a18fed25e3000a0fd88c6a14e9ea27
ColtonPaulBadock <hockey9192008@gmail.com>
2026-02-09 15:20:20 -0800

    Version 6: Added commands and inputs in with buttons for digital and analog for the reverse/forward toggle!

--- source/RcBoatRFControllerSystem/src/main.cpp
+++ source/RcBoatRFControllerSystem/src/main.cpp
@@ -5,7 +5,7 @@
 /*
 Author: Colton Paul Badock
 Date: Feb 2026
-Version: 5
+Version: 6
 
 DESCRIPTION:
 A system to control the boat via recieved radio freqencies.
@@ -21,6 +21,11 @@ There will be a onboard recieve picking up these inputs, it likely will not tran
 //for wireless control of the boat
 RH_ASK user_transmitter;
 
+//INPUT DEVICES
+byte leftTurnButton_port = 2;
+byte rightTurnButton_port = 3;
+byte lightToggleButton_port = 4;
+byte directionToggle = A0;
 
 
 //Transmits a message/command on the
@@ -41,6 +46,16 @@ void transmitMessage(char *msg) {
 //Init function (constructor)
 void setup() {
   
+    //Setup digital and analog pins
+    //based on how we want to use them
+    //for controller inputs
+    pinMode(leftTurnButton_port, INPUT);
+    pinMode(rightTurnButton_port, INPUT);
+    pinMode(lightToggleButton_port, INPUT);
+    pinMode(directionToggle, INPUT_PULLUP);
+
+
+
     //Start serial communication
     //back to the PC (if plugged in)
     //for debugging
@@ -57,10 +72,54 @@ void setup() {
 
 //Infinite application loop
 void loop() {
-  
-    transmitMessage("Hello World!");
 
-    delay(5000);
+    //Allow the user to transmit
+    //to the RC Boat with the
+    //controller inputs they press.
+    transmitCommands();
 
-    Serial.println("Successfully transmitted??");
+}
+
+
+
+//Takes all inputs from the user on the
+//controller that could have occured this cycle,
+//then transmits them on the frequency
+//for the RC Boat to detect.
+void transmitCommands() {
+
+    //Check for given input parameters,
+    //such as button presses or specific
+    //control/sensor values.
+    //If these values are meet, then we
+    //will transmit a command to the RC
+    //boat realted to the control pressed
+    //-------
+    //If the left turn button is pressed,
+    //tell the boat to power the left motor
+    if (digitalRead(leftTurnButton_port) == 1) {
+        transmitMessage("LEFT_MOTOR_RUN");
+    } else {
+        transmitMessage("LEFT_MOTOR_KILL");
+    }
+    //If the right turn button is pressed,
+    //tell the boat to power the right motor
+    if (digitalRead(rightTurnButton_port) == 1) {
+        transmitMessage("RIGHT_MOTOR_RUN");
+    } else {
+        transmitMessage("RIGHT_MOTOR_KILL");
+    }
+    //If the light toggle is pressed,
+    //toggle the lights
+    if (digitalRead(lightToggleButton_port) == 1) {
+        transmitMessage("TOGGLE_LIGHTS");
+    }
+    //If the analog reverse switch is above
+    //has a specific resistance below half,
+    //then toggle to reverse or forward
+    if (analogRead(directionToggle) > 500) {
+        transmitMessage("ACTIVATE_FORWARD");
+    } else {
+        transmitMessage("ACTIVATE_REVERSE");
+    }
 }
--- source/RcBoatRFRecieverSystem/RcBoatRFRecieverSystem/src/main.cpp
+++ source/RcBoatRFRecieverSystem/RcBoatRFRecieverSystem/src/main.cpp
@@ -5,7 +5,7 @@
 /*
 Author: Colton Paul Badock
 Date: Feb 2026
-Version: 5
+Version: 6
 
 DESCRIPTION:
 A system to control the boat via recieved radio freqencies.
